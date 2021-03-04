a4c3e0e538d8985ce90ff443d5671c532426a721
KabirJaipal <73753258+KabirJaipal@users.noreply.github.com>
2021-03-05 01:36:13 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,37 +3,26 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: "Display all commands and descriptions of 𝙎𝙒メ MUSIC",
-  execute(client ,message , args) {
-    var allcmds = "";
+  description: "Display all commands and descriptions of 𝙎𝙒メ MUSIC ",
+  execute(message) {
+    let commands = message.client.commands.array();
 
-    client.commands.forEach(cmd => {
-      let cmdinfo = cmd.info
-      allcmds += "`" + client.config.prefix + cmdinfo.name + " " + cmdinfo.usage + "` ~ " + cmdinfo.description + "\n"
-    })
+    let helpEmbed = new MessageEmbed()
+      .setTitle(`${message.client.user.username} Help`)
+      .setDescription("List of all commands")
+      .setColor("#F8AA2A")
+      .setFooter(`Bot Made By Kabir Jaipal #2021 | github.com/kabirjaipal`);
 
-    let embed = new MessageEmbed()
-      .setAuthor("Commands of " + client.user.username, "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
-      .setColor("Random")
-      .setDescription(allcmds)
-      .setFooter(`To get info of each command you can do ${client.config.prefix}help [command] | Made by Kabir Jaipal#2021`)
+    commands.forEach((cmd) => {
+      helpEmbed.addField(
+        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
+        `${cmd.description}`,
+        true
+      );
+    });
 
-    if (!args[0]) return message.channel.send(embed)
-    else {
-      let cmd = args[0]
-      let command = client.commands.get(cmd)
-      if (!command) command = client.commands.find(x => x.info.aliases.includes(cmd))
-      if (!command) return message.channel.send("Unknown Command")
-      let commandinfo = new MessageEmbed()
-        .setTitle("Command: " + command.info.name + " info")
-        .setColor("YELLOW")
-        .setDescription(`
-Name: ${command.info.name}
-Description: ${command.info.description}
-Usage: \`\`${client.config.prefix}${command.info.name} ${command.info.usage}\`\`
-Aliases: ${command.info.aliases.join(", ")}
-`)
-      message.channel.send(commandinfo)
-    }
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
   }
 };
